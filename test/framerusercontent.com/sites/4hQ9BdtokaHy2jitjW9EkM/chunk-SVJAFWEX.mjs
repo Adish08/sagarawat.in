7a91121331a6eea7978f55c34bc8d7458aefb73a
TheLegendaryPhoenix <adish.sag@gmail.com>
2024-06-29 18:37:32 +0530

    framer test

---
+++ test/framerusercontent.com/sites/4hQ9BdtokaHy2jitjW9EkM/chunk-SVJAFWEX.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"Agency Framer Template",favicon:"https://framerusercontent.com/assets/Vd0YnOL1KQEsLCA9ViMvMiF8xEc.png",socialImage:"https://framerusercontent.com/assets/cebHnK9dCbstXhmHE1udbPEzZ4s.jpg",title:"Newport"}}export{t as a};
+//# sourceMappingURL=chunk-SVJAFWEX.mjs.map
